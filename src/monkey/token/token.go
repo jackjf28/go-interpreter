41e080df9388eef30eeddd374f67865a5ac79062
Jack Farrell <jackfarrell3030@gmail.com>
2025-01-13 21:52:11 -0600

    complete base evaluator, add string support

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -12,8 +12,9 @@ const (
 	EOF     = "EOF"
 
 	// Identifiers + literals
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 143242
+	IDENT  = "IDENT" // add, foobar, x, y, ...
+	INT    = "INT"   // 143242
+	STRING = "STRING"
 
 	// Operators
 	ASSIGN   = "="
